e6b7879e43a6c67f2a4407b81362f2651074da02
igor alex <igoralex8701@gmail.com>
2022-02-03 16:04:57 -0300

    resolvendo ex de js

---
+++ 01-fundamentos/4-bloco-introducao-js-logica/1-dia-js-primeiros-passos/js-operadores-logicos/ex-switch-case.js
@@ -0,0 +1,17 @@
+const status = 'reprovado';
+
+switch (status){
+
+    case 'aprovado':
+        console.log("você foi aprovado")
+        break;
+    case 'aprovado':
+        console.log("você esta na lista de espera")
+        break;    
+    
+    case 'reprovado':
+        console.log("você foi reprovado")
+        break;
+    default:
+        console.log("valor invalido")
+}

93e9e58b1d4479df7709f212012c68857692951e
igor alex <igoralex8701@gmail.com>
2022-02-03 12:31:13 -0300

    ex js variaveis

---
+++ 01-fundamentos/4-bloco-introducao-js-logica/1-dia-js-primeiros-passos/js-variaveis/ex-variaveis.js
@@ -0,0 +1,11 @@
+const myName = "Igor";
+const birthCity = "Lorena-sp";
+let birthYear = "28/08/2019";
+
+console.log(myName + " " + birthCity + " " + birthYear);
+
+let birthYear = "28/08/2030";
+
+console.log(myName + " " + birthCity + " " + birthYear);
+
+

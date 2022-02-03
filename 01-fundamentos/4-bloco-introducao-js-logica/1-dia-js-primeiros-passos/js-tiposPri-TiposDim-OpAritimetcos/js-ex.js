8db6db1d0f501485f8df7a0cb70d3d23dc36e8e2
igor alex <igoralex8701@gmail.com>
2022-02-03 12:42:01 -0300

    js ex area e perimeter

---
+++ 01-fundamentos/4-bloco-introducao-js-logica/1-dia-js-primeiros-passos/js-tiposPri-TiposDim-OpAritimetcos/js-ex.js
@@ -0,0 +1,11 @@
+const base = 5;
+const height = 8
+const area = base*height
+console.log("area = " + area) 
+//-----------------------------------//
+const ladoA = "3"
+const ladoB = "3"
+const ladoC = "5"
+const perimeter = ladoA + ladoB + ladoC
+
+console.log("perimeter: " + perimeter)

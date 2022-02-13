cfe06d3192749f515b6d4e94f493ef12b8085154
igor alex <igoralex8701@gmail.com>
2022-02-13 16:03:10 -0300

    ex4 e 5

--- 01-fundamentos/05-bloco-dom-seletores-eventos-web-storage/03-eventos/script.js
+++ 01-fundamentos/05-bloco-dom-seletores-eventos-web-storage/03-eventos/script.js
@@ -11,25 +11,12 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
-
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-// 4.1. Que tal redirecionar para seu portfólio?
-
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
-
-// Segue abaixo um exemplo do uso de event.target:
-//let li = document.querySelector("#second-li")
 let divUl = document.querySelector(".container");
 
 let divLi0 = divUl.children[0];
 let divLi1 = divUl.children[1];
 let divLi2 = divUl.children[2];
-
+ 
 divLi0.addEventListener("click", function(){
   divLi0.className = "tech"
   divLi1.className = ""
@@ -50,6 +37,48 @@ divLi2.addEventListener("click", function(){
   divLi1.className = ""
   divLi0.className = ""
 });
+
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+
+let inputText = document.querySelector("#input")
+
+    inputText.addEventListener("change", function(){
+      let divLi = document.querySelector(".tech");
+      divLi.innerHTML = inputText.value;
+    });
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+// 4.1. Que tal redirecionar para seu portfólio?
+let idTop3 = document.querySelector("#my-spotrybefy")
+let click = 0
+idTop3.addEventListener("click",contaClick)
+
+function contaClick(){
+  click += 1
+  console.log(click)
+  if(click === 3){
+    window.location.href = "https://igor-alex-castro.github.io/"
+  
+    click = 0
+  }
+ 
+}
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+  idTop3.addEventListener("mouseover", function(){
+    idTop3.style.color = "red"
+  })
+
+  idTop3.addEventListener("mouseout", function(){
+    idTop3.style.color = "white"
+  })
+
+
+
+// Segue abaixo um exemplo do uso de event.target:
+//let li = document.querySelector("#second-li")
 
 
 
